5823adcb025a07de423faca3fe2d29b72ecf7d97
dario <dario.klepoch@freenet.de>
2021-01-15 11:37:38 +0100

    initialized with flutter app with drawing canvas

---
+++ android/app/src/main/kotlin/com/example/da_kanji_recognizer_mobile/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.da_kanji_recognizer_mobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'draw_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Draw(),
+    );
+  }
+}
