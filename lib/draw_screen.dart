1e19911e1a9d7f67db811248bb3a643acc37f409
dario <dario.klepoch@freenet.de>
2021-01-15 15:56:24 +0100

    cleaned: unused elements

--- lib/draw_screen.dart
+++ lib/draw_screen.dart
@@ -1,7 +1,10 @@
-import 'dart:io';
+import 'dart:io' show Platform;
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+
+import 'DrawingPoints.dart';
+import 'DrawingPainter.dart';
 
 class Draw extends StatefulWidget {
   @override
@@ -10,102 +13,90 @@ class Draw extends StatefulWidget {
 
 class _DrawState extends State<Draw> {
   Color selectedColor = Colors.black;
-  Color pickerColor = Colors.black;
   double strokeWidth = 3.0;
   List<DrawingPoints> points = List();
-  bool showBottomList = false;
-  double opacity = 1.0;
   StrokeCap strokeCap = (Platform.isAndroid) ? StrokeCap.butt : StrokeCap.round;
-  SelectedMode selectedMode = SelectedMode.StrokeWidth;
-  List<Color> colors = [
-    Colors.red,
-    Colors.green,
-    Colors.blue,
-    Colors.amber,
-    Colors.black
-  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Container(
-            padding: const EdgeInsets.only(left: 8.0, right: 8.0),
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(50.0),
-                color: Colors.greenAccent),
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      IconButton(
-                          icon: Icon(Icons.undo),
-                          onPressed: () {
-                            setState(() {
-                              showBottomList = false;
-                              points.clear();
-                            });
-                          }),
-                      IconButton(
-                          icon: Icon(Icons.clear),
-                          onPressed: () {
-                            setState(() {
-                              showBottomList = false;
-                              points.clear();
-                            });
-                          }),
-                    ],
-                  ),
-                ],
-              ),
-            )),
-      ),
+        bottomNavigationBar: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Container(
+              padding: const EdgeInsets.only(left: 8.0, right: 8.0),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(50.0),
+                  color: Colors.greenAccent),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        // undo button
+                        IconButton(
+                            icon: Icon(Icons.undo),
+                            onPressed: () {
+                              setState(() {
+                                points.removeLast();
+                              });
+                            }),
+
+                        // clear button
+                        IconButton(
+                            icon: Icon(Icons.clear),
+                            onPressed: () {
+                              setState(() {
+                                points.clear();
+                              });
+                            }),
+                      ],
+                    ),
+                  ],
+                ),
+              )),
+        ),
         body: Column(children: [
           Container(
             width: MediaQuery.of(context).size.width * 3 / 4,
             height: MediaQuery.of(context).size.width * 3 / 4,
             margin: EdgeInsets.all(MediaQuery.of(context).size.width * 1 / 8),
             child: GestureDetector(
-        onPanUpdate: (details) {
-          setState(() {
-            RenderBox renderBox = context.findRenderObject();
-            points.add(DrawingPoints(
-                points: renderBox.globalToLocal(details.globalPosition),
-                paint: Paint()
-                  ..strokeCap = strokeCap
-                  ..isAntiAlias = true
-                  ..color = selectedColor.withOpacity(opacity)
-                  ..strokeWidth = strokeWidth));
-          });
-        },
-        onPanStart: (details) {
-          setState(() {
-            RenderBox renderBox = context.findRenderObject();
-            points.add(DrawingPoints(
-                points: renderBox.globalToLocal(details.globalPosition),
-                paint: Paint()
-                  ..strokeCap = strokeCap
-                  ..isAntiAlias = true
-                  ..color = selectedColor.withOpacity(opacity)
-                  ..strokeWidth = strokeWidth));
-          });
-        },
-        onPanEnd: (details) {
-          setState(() {
-            points.add(null);
-          });
-        },
-        child: CustomPaint(
-          size: Size.infinite,
-          painter: DrawingPainter(
-            pointsList: points,
-          ),
-        ),
-      ),
+              onPanUpdate: (details) {
+                setState(() {
+                  RenderBox renderBox = context.findRenderObject();
+                  points.add(DrawingPoints(
+                      points: renderBox.globalToLocal(details.globalPosition),
+                      paint: Paint()
+                        ..strokeCap = strokeCap
+                        ..isAntiAlias = true
+                        ..strokeWidth = strokeWidth));
+                });
+              },
+              onPanStart: (details) {
+                setState(() {
+                  RenderBox renderBox = context.findRenderObject();
+                  points.add(DrawingPoints(
+                      points: renderBox.globalToLocal(details.globalPosition),
+                      paint: Paint()
+                        ..strokeCap = strokeCap
+                        ..isAntiAlias = true
+                        ..strokeWidth = strokeWidth));
+                });
+              },
+              onPanEnd: (details) {
+                setState(() {
+                  points.add(null);
+                });
+              },
+              child: CustomPaint(
+                painter: DrawingPainter(
+                  pointsList: points,
+                ),
+              ),
+            ),
           ),
           Row(children: [
             Expanded(child: FlatButton(onPressed: () {}, child: Text("漢字 1"))),
@@ -122,12 +113,5 @@ class _DrawState extends State<Draw> {
             Expanded(child: FlatButton(onPressed: () {}, child: Text("漢字 10")))
           ]),
         ]));
+  }
 }
-
-class DrawingPoints {
-  Paint paint;
-  Offset points;
-  DrawingPoints({this.points, this.paint});
-}
-
-enum SelectedMode { StrokeWidth, Opacity, Color }
