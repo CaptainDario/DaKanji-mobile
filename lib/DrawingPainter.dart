6b7f90f2e53b91578965d546fe7ca49a46324748
dario <dario.klepoch@freenet.de>
2021-01-16 22:19:16 +0100

    added added drawing background to canvas

--- lib/DrawingPainter.dart
+++ lib/DrawingPainter.dart
@@ -1,4 +1,4 @@
-import 'dart:ui';
+import 'dart:ui' as ui;
 
 import 'package:flutter/material.dart';
 
@@ -11,10 +11,11 @@ class DrawingPainter extends CustomPainter {
 
   @override
   void paint(Canvas canvas, Size size) {
-    print(size);
     canvas.clipRect(Rect.fromLTWH(0, 0, size.width, size.height));
     //set the background color of the canvas
     canvas.drawColor(Color.fromARGB(255, 0, 0, 255), BlendMode.srcATop);
+
+    this.paintKanjiDrawingAid(canvas, size);
 
     for (int i = 0; i < pointsList.length - 1; i++) {
       if (pointsList[i] != null && pointsList[i + 1] != null) {
@@ -25,8 +26,33 @@ class DrawingPainter extends CustomPainter {
         offsetPoints.add(pointsList[i].points);
         offsetPoints.add(Offset(
             pointsList[i].points.dx + 0.1, pointsList[i].points.dy + 0.1));
-        canvas.drawPoints(PointMode.points, offsetPoints, pointsList[i].paint);
+        canvas.drawPoints(
+            ui.PointMode.points, offsetPoints, pointsList[i].paint);
       }
+    }
+  }
+
+  void paintKanjiDrawingAid(Canvas canvas, Size size) {
+    int dash_amount = 20;
+    int dashes = (size.width / (dash_amount + 1.0)).floor();
+
+    // square
+    Paint paint = Paint()
+      ..strokeWidth = 6.0
+      ..style = PaintingStyle.stroke;
+    canvas.drawRect(Rect.fromLTWH(0, 0, size.width, size.height), paint);
+    // vertical
+    paint.strokeWidth = 3.0;
+    for (int i = 0; i < dash_amount + 1; i++) {
+      if (i % 2 == 1) continue;
+      canvas.drawLine(Offset(size.width / 2.0, dashes * (i * 1.0)),
+          Offset(size.width / 2.0, dashes * (i + 1.0)), paint);
+    }
+    // horizontal
+    for (int i = 0; i < dash_amount + 1; i++) {
+      if (i % 2 == 1) continue;
+      canvas.drawLine(Offset(dashes * (i * 1.0), size.width / 2.0),
+          Offset(dashes * (i + 1.0), size.width / 2.0), paint);
     }
   }
 
