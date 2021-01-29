c38d5d9df86aabffdd2b38cff8cedc63b9181588
dario <dario.klepoch@freenet.de>
2021-01-29 22:38:15 +0100

    Added settings to open prediction in dicts

--- lib/SettingsScreen.dart
+++ lib/SettingsScreen.dart
@@ -1,8 +1,9 @@
 import 'dart:io' show Platform;
-import 'dart:ui';
+
+import 'package:flutter/material.dart';
 
 import 'package:da_kanji_recognizer_mobile/DaKanjiRecognizerDrawer.dart';
-import 'package:flutter/material.dart';
+import 'globals.dart';
 
 class SettingsScreen extends StatefulWidget {
   @override
@@ -10,24 +11,95 @@ class SettingsScreen extends StatefulWidget {
 }
 
 class _SettingsScreenState extends State<SettingsScreen> {
-  bool openInJisho = false;
+  void toggleSwitches(int switchToBeTrue) {
+    SETTINGS.setTogglesToFalse();
+
+    switch (switchToBeTrue) {
+      case 1:
+        SETTINGS.openWithJisho = true;
+        break;
+
+      case 2:
+        SETTINGS.openWithTakoboto = true;
+        break;
+
+      case 3:
+        SETTINGS.openWithWadoku = true;
+        break;
+
+      case 4:
+        SETTINGS.openWithCustomURL = true;
+        break;
+    }
+
+    SETTINGS.save();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(title: Text("Settings")),
         drawer: DaKanjiRecognizerDrawer(),
-        body: ListView(padding: EdgeInsets.zero, children: <Widget>[
-          ListTile(
-              title: Text("Open in Jisho"),
-              trailing: Switch(
-                  value: openInJisho,
-                  onChanged: (value) {
-                    setState(() {
-                      openInJisho = value;
-                    });
-                  }),
-              onTap: () {}),
-        ]));
+        body: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            ListTile(
+                title: Text("Long press opens on jisho.org"),
+                trailing: Switch(
+                    value: SETTINGS.openWithJisho,
+                    onChanged: (value) {
+                      setState(() {
+                        toggleSwitches(1);
+                      });
+                    }),
+                onTap: () {}),
+            ListTile(
+                title: Text("Long press opens on takoboto.jp"),
+                trailing: Switch(
+                    value: SETTINGS.openWithTakoboto,
+                    onChanged: (value) {
+                      setState(() {
+                        toggleSwitches(2);
+                      });
+                    }),
+                onTap: () {}),
+            ListTile(
+                title: Text("Long press opens on wadoku.de"),
+                trailing: Switch(
+                    value: SETTINGS.openWithWadoku,
+                    onChanged: (value) {
+                      setState(() {
+                        toggleSwitches(3);
+                      });
+                    }),
+                onTap: () {}),
+            ListTile(
+                title: Text("Long press opens a custom URL"),
+                trailing: Switch(
+                    value: SETTINGS.openWithCustomURL,
+                    onChanged: (value) {
+                      setState(() {
+                        toggleSwitches(4);
+                      });
+                    }),
+                onTap: () {}),
+            ListTile(
+                title: Row(children: [
+                  Container(
+                      child: Expanded(
+                          child: TextField(
+                    enabled: SETTINGS.openWithCustomURL,
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: SETTINGS.customURL),
+                    onChanged: (value) {
+                      SETTINGS.customURL = value;
+                      SETTINGS.save();
+                    },
+                  )))
+                ]),
+                onTap: () {}),
+          ],
+        ));
   }
 }
