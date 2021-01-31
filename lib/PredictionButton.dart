8f37a46e01abe7b7b7c7593a49caa39ab012dbef
dario <dario.klepoch@freenet.de>
2021-01-31 21:52:09 +0100

    aded placeholder for pres and long press

--- lib/PredictionButton.dart
+++ lib/PredictionButton.dart
@@ -10,20 +10,28 @@ class PredictionButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Expanded(
-        child: AspectRatio(
-            aspectRatio: 1,
-            child: Container(
-                margin: EdgeInsets.all(5),
-                padding: EdgeInsets.all(0),
-                child: RaisedButton(
-                    color: Colors.white.withAlpha(50),
-                    padding: EdgeInsets.all(0),
-                    onPressed: () {},
-                    child: FittedBox(
-                        child: Text(
-                      this.char,
-                      textAlign: TextAlign.center,
-                      style: new TextStyle(fontSize: 1000.0),
-                    ))))));
+      child: AspectRatio(
+        aspectRatio: 1,
+        child: Container(
+          margin: EdgeInsets.all(5),
+          padding: EdgeInsets.all(0),
+          child: MaterialButton(
+            color: Colors.white.withAlpha(50),
+            padding: EdgeInsets.all(0),
+            // copy the character to clipboar on single press
+            onPressed: () => {print("Pressed " + this.char)},
+            // open the prediction in a dictionary (set in settings)
+            onLongPress: () => {print("Long pressed " + this.char)},
+            child: FittedBox(
+              child: Text(
+                this.char,
+                textAlign: TextAlign.center,
+                style: new TextStyle(fontSize: 1000.0),
+              )
+            )
+          )
+        )
+      )
+    );
   }
 }
