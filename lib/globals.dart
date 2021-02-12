f0d6b7c6f4bc4710eee6962c2f25956ca1542e7b
dario <dario.klepoch@freenet.de>
2021-02-12 21:34:17 +0100

    added assets paths, interpreter and title

--- lib/globals.dart
+++ lib/globals.dart
@@ -1,5 +1,17 @@
 library my_prj.globals;
 
+import 'package:tflite_flutter/tflite_flutter.dart';
+
 import 'Settings.dart';
 
+String appTitle = "DaKanjiRecognizer";
+
 Settings SETTINGS = new Settings();
+
+// Assets
+String LABELS_ASSET = 'assets/labels_CNN_kanji_only.txt';
+String CNN_KANJI_ONLY_ASSET = 'model_CNN_kanji_only.tflite';
+
+// inference 
+List<String> LABEL_LIST;
+Interpreter CNN_KANJI_ONLY_INTERPRETER;
