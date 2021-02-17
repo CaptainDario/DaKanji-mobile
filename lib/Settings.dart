e9d110ea8f21ad3334a0c018369c15fccd4362e1
dario <dario.klepoch@freenet.de>
2021-02-17 18:56:52 +0100

    changed takoboto to default translators

--- lib/Settings.dart
+++ lib/Settings.dart
@@ -26,14 +26,14 @@ class Settings {
   /// Indicates if a long press will use the jisho URL.
   bool openWithJisho;
 
-  /// Indicates if a long press will use the takoboto URL.
-  bool openWithTakoboto;
-
   /// Indicates if a long press will use the wadoku URL.
   bool openWithWadoku;
 
   /// Indicates if a long press will use the weblio URL.
   bool openWithWeblio;
+  
+  /// Indicates if a long press will use the default translator.
+  bool openWithDefaultTranslator;
 
   /// The theme which the application will use.
   /// System will match the settings of the system.
@@ -55,7 +55,6 @@ class Settings {
     String kanjiPlaceholder = "%X%";
 
     jishoURL = "https://jisho.org/search/" + kanjiPlaceholder + "%23kanji";
-    takobotoURL = "http://takoboto.jp/?q=" + kanjiPlaceholder;
     wadokuURL = "https://www.wadoku.de/search/" + kanjiPlaceholder;
     weblioURL = "https://www.weblio.jp/content/" + kanjiPlaceholder;
   }
@@ -66,19 +65,23 @@ class Settings {
   String openWithSelectedDictionary(String kanji) {
     String url;
 
+    // determine which URL should be used for finding the character
     if (openWithCustomURL)
       url = customURL;
     else if (openWithJisho)
       url = jishoURL;
-    else if (openWithTakoboto)
-      url = takobotoURL;
     else if (openWithWadoku)
       url = wadokuURL;
-    else if (openWithWeblio) url = weblioURL;
+    else if (openWithWeblio)
+      url = weblioURL;
+    else if (openWithDefaultTranslator)
+      url = takobotoURL;
 
+    // check that the url starts with protocol, otherwise launch(fails)
     if (!(url.startsWith("http://") || url.startsWith("https://")))
       url = "https://" + url;
 
+    // replace the placeholder with the actual character
     url = url.replaceFirst(new RegExp(kanjiPlaceholder), kanji);
     url = Uri.encodeFull(url);
     return url;
@@ -88,9 +91,9 @@ class Settings {
   void setTogglesToFalse() {
     openWithCustomURL = false;
     openWithJisho = false;
-    openWithTakoboto = false;
     openWithWadoku = false;
     openWithWeblio = false;
+    openWithDefaultTranslator = false;
   }
 
   /// Saves all settings to the SharedPreferences.
@@ -103,7 +106,7 @@ class Settings {
     // set value
     prefs.setBool('openWithCustomURL', openWithCustomURL);
     prefs.setBool('openWithJisho', openWithJisho);
-    prefs.setBool('openWithTakoboto', openWithTakoboto);
+    prefs.setBool('openWithDefaultTranslator', openWithDefaultTranslator);
     prefs.setBool('openWithWadoku', openWithWadoku);
     prefs.setBool('openWithWeblio', openWithWeblio);
     prefs.setString('customURL', customURL);
@@ -116,22 +119,25 @@ class Settings {
 
     openWithCustomURL = await loadOpenWithCustomURL();
     openWithJisho = await loadOpenWithJisho();
-    openWithTakoboto = await loadOpenWithTakoboto();
+    openWithDefaultTranslator = await loadOpenWithTakoboto();
     openWithWadoku = await loadOpenWithWadoku();
     openWithWeblio = await loadOpenWithWeblio();
     customURL = await loadCustomURL();
     selectedTheme = await loadSelectedTheme();
 
-    // assure that atleast one switch is set to true
+    // assure that at least one switch is set to true
     if (!this.openWithCustomURL &&
         !this.openWithJisho &&
-        !this.openWithTakoboto &&
+        !this.openWithDefaultTranslator &&
         !this.openWithWadoku &&
         !this.openWithWeblio) {
       this.openWithJisho = true;
     }
   }
 
+  /// Loads the setting state for the "OpenWithCustomURL" setting
+  ///
+  /// @returns true if the setting was enable, false otherwise
   Future<bool> loadOpenWithCustomURL() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     bool loaded = prefs.getBool('openWithCustomURL') ?? false;
@@ -139,6 +145,9 @@ class Settings {
     return loaded;
   }
 
+  /// Loads the setting state for the "OpenWithJisho" setting
+  ///
+  /// @returns true if the setting was enable, false otherwise
   Future<bool> loadOpenWithJisho() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     bool loaded = prefs.getBool('openWithJisho') ?? false;
@@ -146,13 +155,19 @@ class Settings {
     return loaded;
   }
 
+  /// Loads the setting state for the "OpenWithTakoboto" setting
+  ///
+  /// @returns true if the setting was enable, false otherwise
   Future<bool> loadOpenWithTakoboto() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
-    bool loaded = prefs.getBool('openWithTakoboto') ?? false;
+    bool loaded = prefs.getBool('openWithDefaultTranslator') ?? false;
 
     return loaded;
   }
 
+  /// Loads the setting state for the "OpenWithWadoku" setting
+  ///
+  /// @returns true if the setting was enable, false otherwise
   Future<bool> loadOpenWithWadoku() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     bool loaded = prefs.getBool('openWithWadoku') ?? false;
@@ -160,6 +175,9 @@ class Settings {
     return loaded;
   }
 
+  /// Loads the setting state for the "OpenWithWeblio" setting
+  ///
+  /// @returns true if the setting was enable, false otherwise
   Future<bool> loadOpenWithWeblio() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     bool loaded = prefs.getBool("openWithWeblio") ?? false;
@@ -167,6 +185,9 @@ class Settings {
     return loaded;
   }
 
+  /// Loads the value for the custom URL.
+  ///
+  /// @returns The custom URL
   Future<String> loadCustomURL() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     String loaded = prefs.getString('customURL') ?? "";
@@ -174,6 +195,9 @@ class Settings {
     return loaded;
   }
 
+  /// Loads the selected theme
+  ///
+  /// @returns the selected theme
   Future<String> loadSelectedTheme() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     String loaded = prefs.getString('selectedTheme') ?? "system";
