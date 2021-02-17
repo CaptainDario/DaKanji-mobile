243e4344ce200148545818a3cfe2e93df802b248
dario <dario.klepoch@freenet.de>
2021-02-17 18:56:17 +0100

    changed takoboto to default dictionary

--- lib/SettingsScreen.dart
+++ lib/SettingsScreen.dart
@@ -11,26 +11,26 @@ class SettingsScreen extends StatefulWidget {
 class _SettingsScreenState extends State<SettingsScreen> {
   void toggleSwitches(int switchToBeTrue) {
     SETTINGS.setTogglesToFalse();
-
+    // only one dictionary setting should be true at a time
     switch (switchToBeTrue) {
       case 1:
-        SETTINGS.openWithCustomURL = true;
-        break;
-
-      case 2:
         SETTINGS.openWithJisho = true;
         break;
 
-      case 3:
-        SETTINGS.openWithTakoboto = true;
-        break;
-
-      case 4:
+      case 2:
         SETTINGS.openWithWadoku = true;
         break;
 
-      case 5:
+      case 3:
         SETTINGS.openWithWeblio = true;
+        break;
+      
+      case 4:
+        SETTINGS.openWithCustomURL = true;
+        break;
+      
+      case 5:
+        SETTINGS.openWithDefaultTranslator = true;
         break;
     }
 
@@ -40,100 +40,109 @@ class _SettingsScreenState extends State<SettingsScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text("Settings")),
-        drawer: DaKanjiRecognizerDrawer(),
-        // ListView of all available settings
-        body: ListView(
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-            // different options for dictionary on long press
-            ListTile(title: Text("Long press opens:")),
-            ListTile(
-                title: Text("on jisho.org"),
-                trailing: Switch(
-                    value: SETTINGS.openWithJisho,
-                    onChanged: (value) {
-                      setState(() {
-                        toggleSwitches(2);
-                      });
-                    }),
-                onTap: () {}),
-            ListTile(
-                title: Text("on takoboto.jp"),
-                trailing: Switch(
-                    value: SETTINGS.openWithTakoboto,
-                    onChanged: (value) {
-                      setState(() {
-                        toggleSwitches(3);
-                      });
-                    }),
-                onTap: () {}),
-            ListTile(
-                title: Text("on wadoku.de"),
-                trailing: Switch(
-                    value: SETTINGS.openWithWadoku,
-                    onChanged: (value) {
-                      setState(() {
-                        toggleSwitches(4);
-                      });
-                    }),
-                onTap: () {}),
-            ListTile(
-                title: Text("on weblio.de"),
-                trailing: Switch(
-                    value: SETTINGS.openWithWeblio,
-                    onChanged: (value) {
-                      setState(() {
-                        toggleSwitches(5);
-                      });
-                    }),
-                onTap: () {}),
-            // let the user enter a custom url for flexibility
-            ListTile(
-                title: Text("a custom URL"),
-                trailing: Switch(
-                    value: SETTINGS.openWithCustomURL,
-                    onChanged: (value) {
-                      setState(() {
-                        toggleSwitches(1);
-                      });
-                    }),
-                onTap: () {}),
-            ListTile(
-                title: Row(children: [
-                  Container(
-                      child: Expanded(
-                          child: TextField(
-                    enabled: SETTINGS.openWithCustomURL,
-                    decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: SETTINGS.customURL),
-                    onChanged: (value) {
-                      SETTINGS.customURL = value;
-                      SETTINGS.save();
-                    },
-                  )))
-                ]),
-                onTap: () {}),
-            // setting for which theme to use
-            ListTile(
-                title: Text("theme (used at next app start)"),
-                trailing: DropdownButton<String>(
-                  value: SETTINGS.selectedTheme,
-                  items: SETTINGS.themes
-                      .map<DropdownMenuItem<String>>((String value) {
-                    return DropdownMenuItem<String>(
-                        value: value, child: Text(value));
-                  }).toList(),
-                  onChanged: (String newValue) {
+      appBar: AppBar(title: Text("Settings")),
+      drawer: DaKanjiRecognizerDrawer(),
+      // ListView of all available settings
+      body: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          // different options for dictionary on long press
+          ListTile(title: Text("Long press opens:")),
+          ListTile(
+              title: Text("on jisho.org"),
+              trailing: Switch(
+                  value: SETTINGS.openWithJisho,
+                  onChanged: (value) {
                     setState(() {
-                      SETTINGS.selectedTheme = newValue;
-                      SETTINGS.save();
+                      toggleSwitches(1);
                     });
-                  },
-                ),
-                onTap: () {}),
-          ],
-        ));
+                  }),
+              onTap: () {}),
+          ListTile(
+              title: Text("on wadoku.de"),
+              trailing: Switch(
+                  value: SETTINGS.openWithWadoku,
+                  onChanged: (value) {
+                    setState(() {
+                      toggleSwitches(2);
+                    });
+                  }),
+              onTap: () {}),
+          ListTile(
+              title: Text("on weblio.de"),
+              trailing: Switch(
+                  value: SETTINGS.openWithWeblio,
+                  onChanged: (value) {
+                    setState(() {
+                      toggleSwitches(3);
+                    });
+                  }),
+              onTap: () {}),
+          // let the user enter a custom url for flexibility
+          ListTile(
+              title: Text("a custom URL"),
+              trailing: Switch(
+                  value: SETTINGS.openWithCustomURL,
+                  onChanged: (value) {
+                    setState(() {
+                      toggleSwitches(4);
+                    });
+                  }),
+              onTap: () {}),
+          ListTile(
+            title: Row(
+              children: [
+                Container(
+                  child: Expanded(
+                    child: TextField(
+                      enabled: SETTINGS.openWithCustomURL,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: SETTINGS.customURL,
+                        hintText: "dictionary URL ex.:" +
+                          "http://takoboto.jp/?q=%X%'"), 
+                      onChanged: (value) {
+                        SETTINGS.customURL = value;
+                        SETTINGS.save();
+                      },
+                    )
+                  )
+                )
+              ]
+            ),
+              onTap: () {}),
+          // setting for which theme to use
+          ListTile(
+              title: Text("theme (used at next app start)"),
+              trailing: DropdownButton<String>(
+                value: SETTINGS.selectedTheme,
+                items: SETTINGS.themes
+                    .map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                      value: value, child: Text(value));
+                }).toList(),
+                onChanged: (String newValue) {
+                  setState(() {
+                    SETTINGS.selectedTheme = newValue;
+                    SETTINGS.save();
+                  });
+                },
+              ),
+              onTap: () {}
+          ),
+          ListTile(
+              title: Text("translate dialogue"),
+              trailing: Switch(
+                  value: SETTINGS.openWithDefaultTranslator,
+                  onChanged: (value) {
+                    setState(() {
+                      toggleSwitches(5);
+                    });
+                  }),
+              onTap: () {}
+          ),
+        ],
+      )
+    );
   }
 }
