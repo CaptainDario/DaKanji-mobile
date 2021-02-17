60e50105e2eb522a6ddf0b67874b671f154af265
dario <dario.klepoch@freenet.de>
2021-02-17 13:50:17 +0100

    run inference on GPU

--- lib/globals.dart
+++ lib/globals.dart
@@ -1,10 +1,13 @@
 library my_prj.globals;
 
+import 'package:device_info/device_info.dart';
 import 'package:tflite_flutter/tflite_flutter.dart';
 
 import 'Settings.dart';
 
 String appTitle = "DaKanjiRecognizer";
+
+String usedBackend = "";
 
 Settings SETTINGS = new Settings();
 
@@ -12,6 +15,6 @@ Settings SETTINGS = new Settings();
 String LABELS_ASSET = 'assets/labels_CNN_kanji_only.txt';
 String CNN_KANJI_ONLY_ASSET = 'model_CNN_kanji_only.tflite';
 
-// inference 
+// inference
 List<String> LABEL_LIST;
 Interpreter CNN_KANJI_ONLY_INTERPRETER;
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'dart:io' show Platform;
+import 'package:device_info/device_info.dart';
 
 import 'Settingsscreen.dart';
 import 'DrawScreen.dart';
@@ -24,10 +26,60 @@ Future<void> init() async {
   String labels = await rootBundle.loadString(LABELS_ASSET);
   LABEL_LIST = labels.split("");
 
-  // initialize interpreter(s)
-  CNN_KANJI_ONLY_INTERPRETER =
-      await Interpreter.fromAsset("model_CNN_kanji_only.tflite");
+  initInterpreter();
+}
 
+void initInterpreter() async {
+  // get platform information
+  DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
+
+  if (Platform.isAndroid) {
+    AndroidDeviceInfo androidInfo = await deviceInfo.androidInfo;
+
+    // if the application is running not in an emulator
+    if (androidInfo.isPhysicalDevice) {
+      InterpreterOptions interpreterOptions;
+
+      // use NNAPI on android if android API >= 27
+      if (androidInfo.version.sdkInt >= 27) {
+      usedBackend = "Android NNAPI Delegate";
+        interpreterOptions = InterpreterOptions()..useNnApiForAndroid = true;
+      }
+      // otherwise fallback to GPU delegate
+      else {
+      usedBackend = "Android GPU Delegate";
+        final gpuDelegateV2 = GpuDelegateV2(
+            options: GpuDelegateOptionsV2(
+                false,
+                TfLiteGpuInferenceUsage.fastSingleAnswer,
+                TfLiteGpuInferencePriority.minLatency,
+                TfLiteGpuInferencePriority.auto,
+                TfLiteGpuInferencePriority.auto));
+        interpreterOptions = InterpreterOptions()..addDelegate(gpuDelegateV2);
+      }
+
+      // initialize interpreter(s)
+      CNN_KANJI_ONLY_INTERPRETER = await Interpreter.fromAsset(
+          CNN_KANJI_ONLY_ASSET,
+          options: interpreterOptions);
+    }
+  }
+  else if (Platform.isIOS) {
+    usedBackend = "IOS Metal Delegate";
+    final gpuDelegate = GpuDelegate(
+      options: GpuDelegateOptions(true, TFLGpuDelegateWaitType.active),
+    );
+    var interpreterOptions = InterpreterOptions()..addDelegate(gpuDelegate);
+    CNN_KANJI_ONLY_INTERPRETER = await Interpreter.fromAsset(
+        CNN_KANJI_ONLY_ASSET,
+        options: interpreterOptions);
+  }
+  // use cpu inference on emulators
+  else {
+    usedBackend = "CPU";
+    CNN_KANJI_ONLY_INTERPRETER =
+        await Interpreter.fromAsset(CNN_KANJI_ONLY_ASSET);
+  }
 }
 
 class DaKanjiRecognizerApp extends StatelessWidget {
