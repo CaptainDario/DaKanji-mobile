da2d32b424ca11af3e44e9ca90b9ce11179fe816
dario <dario.klepoch@freenet.de>
2021-02-19 16:18:12 +0100

    added tutorial identifiers

--- lib/globals.dart
+++ lib/globals.dart
@@ -1,8 +1,9 @@
 library my_prj.globals;
 
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:tflite_flutter/tflite_flutter.dart';
-import 'dart:io' show Platform;
+import 'package:tutorial_coach_mark/tutorial_coach_mark.dart';
 
 import 'Settings.dart';
 
@@ -15,19 +16,37 @@ const String VERSION = "0.3.0";
 // ignore: non_constant_identifier_names
 final Settings SETTINGS = new Settings();
 
+// the global key for the drawer
+final GlobalKey<ScaffoldState> DRAWER_KEY = GlobalKey();
+
 // showcase view keys
 // ignore: non_constant_identifier_names
 final List<GlobalKey> SHOWCASE_KEYS_DRAWING =
   List.generate(SHOWCASE_TEXTS_DRAWING.length, (i) => GlobalKey());
+const List<String> SHOWCASE_IDENTIFIERS_DRAWING = [
+  "Drawing",
+  "Undo",
+  "Clear",
+  "Predictions",
+  "Short Press",
+  "Long Press",
+  "Dictionary Settings"
+];
 const List<String> SHOWCASE_TEXTS_DRAWING = [
   "Draw a character here.",
   "Press to undo the last stroke.",
   "Erase all strokes",
-  "Here are the predicted characters.",
+  "The predicted characters will be shown here.",
   "A short press copies the prediction.",
   "A long press opens the prediction in a dictionary.",
   "In the settings the dictionary can be chosen."
 ];
+final List<ContentAlign> SHOWCASE_ALIGNS_DRAWING = [
+  ContentAlign.bottom, ContentAlign.bottom, ContentAlign.bottom,
+  ContentAlign.top, ContentAlign.bottom, ContentAlign.bottom,
+  ContentAlign.bottom
+];
+final Color SHOWCASE_VIGNETTE_COLOR = Color.fromARGB(255, 10, 10, 10);
 
 // Assets
 const String LABELS_ASSET = 'assets/labels_CNN_kanji_only.txt';
