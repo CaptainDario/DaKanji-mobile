3a6a6aa0c2d19b73cde5054e2011ac5c9afc08bb
dario <dario.klepoch@freenet.de>
2021-03-11 23:15:52 +0100

    added takoboto to settings

--- lib/Settings.dart
+++ lib/Settings.dart
@@ -24,6 +24,9 @@ class Settings {
   /// Indicates if a long press will use the jisho URL.
   bool openWithJisho;
 
+  /// Indicates if a long press will use the takoboto app.
+  bool openWithTakoboto;
+
   /// Indicates if a long press will use the wadoku URL.
   bool openWithWadoku;
 
@@ -35,7 +38,14 @@ class Settings {
 
   /// A list with all available dictionary options.
   List<String> dictionaries = 
-    ["a custom URL", "systemTranslator", "jisho.org", "wadoku.de", "weblio.jp"];
+    [
+      "jisho (web)", 
+      "wadoku (web)",
+      "weblio (web)",
+      "a custom URL",
+      "systemTranslator",
+      "takoboto (app)"
+    ];
 
   /// The string representation of the dictionary which will be used (long press)
   String selectedDictionary;
@@ -87,7 +97,7 @@ class Settings {
     else if (openWithWeblio)
       url = weblioURL;
 
-    // check that the URL starts with protocol, otherwise launch(fails)
+    // check that the URL starts with protocol, otherwise launch() fails
     if (!(url.startsWith("http://") || url.startsWith("https://")))
       url = "https://" + url;
 
@@ -101,6 +111,7 @@ class Settings {
   void setTogglesToFalse() {
     openWithCustomURL = false;
     openWithJisho = false;
+    openWithTakoboto = false;
     openWithWadoku = false;
     openWithWeblio = false;
     openWithDefaultTranslator = false;
@@ -115,15 +126,17 @@ class Settings {
     selectedDictionary = selection;
 
     if(selection == dictionaries[0])
-      openWithCustomURL = true;
-    else if(selection == dictionaries[1])
-      openWithDefaultTranslator = true;
-    else if(selection == dictionaries[2])
       openWithJisho = true;
-    else if(selection == dictionaries[3])
+    else if(selection == dictionaries[1])
       openWithWadoku = true;
-    else if(selection == dictionaries[4])
+    else if(selection == dictionaries[2])
       openWithWeblio = true;
+    else if(selection == dictionaries[3])
+      openWithCustomURL = true;
+    else if(selection == dictionaries[4])
+      openWithDefaultTranslator = true;
+    else if(selection == dictionaries[5])
+      openWithTakoboto = true;
     else
       print("dictionary undefined");
 
@@ -138,6 +151,7 @@ class Settings {
     prefs.setBool('openWithCustomURL', openWithCustomURL);
     prefs.setBool('openWithJisho', openWithJisho);
     prefs.setBool('openWithDefaultTranslator', openWithDefaultTranslator);
+    prefs.setBool('openWithTakoboto', openWithTakoboto);
     prefs.setBool('openWithWadoku', openWithWadoku);
     prefs.setBool('openWithWeblio', openWithWeblio);
     prefs.setBool('showShowcaseViewDrawing', showShowcaseViewDrawing);
@@ -148,7 +162,7 @@ class Settings {
     prefs.setString('selectedDictionary', selectedDictionary);
 
     // make sure the loaded drop down values are correct
-    if(selectedDictionary == "") selectedDictionary = "jisho.org";
+    if(selectedDictionary == "") selectedDictionary = dictionaries[0];
     if(selectedTheme == "") selectedTheme = themes[0];
   }
 
@@ -157,6 +171,7 @@ class Settings {
     openWithCustomURL = await loadBool('openWithCustomURL');
     openWithJisho = await loadBool('openWithJisho');
     openWithDefaultTranslator = await loadBool('openWithDefaultTranslator');
+    openWithTakoboto = await loadBool('openWithTakoboto');
     openWithWadoku = await loadBool('openWithWadoku');
     openWithWeblio = await loadBool('openWithWeblio');
     showShowcaseViewDrawing = await loadBool('showShowcaseViewDrawing');
