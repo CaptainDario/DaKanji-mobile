695147127a7a1b4ca28b7cf0960bc6c0247c49f4
dario <dario.klepoch@freenet.de>
2021-03-18 20:14:59 +0100

    added a text buffer to serach multiple characters

--- lib/DrawScreen.dart
+++ lib/DrawScreen.dart
@@ -1,10 +1,13 @@
+import 'package:da_kanji_recognizer_mobile/KanjiBuffer.dart';
 import 'package:da_kanji_recognizer_mobile/globals.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:tutorial_coach_mark/tutorial_coach_mark.dart';
 
 import 'package:da_kanji_recognizer_mobile/DaKanjiRecognizerDrawer.dart';
 import 'DrawingPainter.dart';
 import 'PredictionButton.dart';
+import 'KanjiBufferWidget.dart';
 
 class DrawScreen extends StatefulWidget {
   @override
@@ -21,6 +24,8 @@ class _DrawScreenState extends State<DrawScreen> {
   List<String> predictions = List.generate(10, (index) => " ");
   // save the context for the Showcase view
   BuildContext myContext;
+  // buffer for building a word
+  KanjiBuffer kanjiBuffer = KanjiBuffer();
 
   
   // show case
@@ -37,6 +42,11 @@ class _DrawScreenState extends State<DrawScreen> {
       initTargets();
       showTutorial();
     }
+
+    // always rebuild the ui when the kanji buffer changed
+    kanjiBuffer.addListener(() {
+      setState(() { });
+    });
   }
 
   @override
@@ -45,127 +55,149 @@ class _DrawScreenState extends State<DrawScreen> {
     canvas = new DrawingPainter(points, darkMode);
     // init size of canvas and assure that it is min. 20 smaller than screen width
     canvasSize = MediaQuery.of(context).size.height * 3/6;
-    if(canvasSize > MediaQuery.of(context).size.width)
+    if(canvasSize >= MediaQuery.of(context).size.width - 20)
       canvasSize = MediaQuery.of(context).size.width - 20;
 
     return Scaffold(
+      //resizeToAvoidBottomInset: false,
       key: DRAWER_KEY,
       appBar: AppBar(
         title: Text("Drawing"),
       ),
       drawer: DaKanjiRecognizerDrawer(),
       body: Center(
-        child: Column(
-        children: [
-          // the canvas to draw on
-          Container(
-            width: canvasSize,
-            height: canvasSize,
-            margin: EdgeInsets.fromLTRB(0, 
-              (MediaQuery.of(context).size.width - canvasSize) / 2, 
-              0, 0),
-            child: GestureDetector(
-              key: SHOWCASE_KEYS_DRAWING[0],
-              // drawing pointer moved
-              onPanUpdate: (details) {
-                setState(() {
-                  RenderBox renderBox = context.findRenderObject();
-                  Offset point =
-                      renderBox.globalToLocal(details.localPosition);
-                  points[points.length - 1].add(point);
-                });
-              },
-              // started drawing
-              onPanStart: (details) {
-                setState(() {
-                  points.add([]);
-                  RenderBox renderBox = context.findRenderObject();
-                  Offset point =
-                      renderBox.globalToLocal(details.localPosition);
-                  points[points.length - 1].add(point);
-                });
-              },
-              // finished drawing a stroke
-              onPanEnd: (details) async {
-                // remove single points and don't run inference for them
-                if (points[points.length - 1].length == 1)
-                  points.removeLast();
-                else
-                  predictions = await canvas.runInference();
-                setState(() {});
-              },
-              child: CustomPaint(
-                painter: canvas,
+        child: Column( 
+          children: [
+            // the canvas to draw on
+            Container(
+              width: canvasSize,
+              height: canvasSize,
+              margin: EdgeInsets.fromLTRB(0, 
+                (MediaQuery.of(context).size.width - canvasSize) / 2, 
+                0, 0),
+              child: GestureDetector(
+                key: SHOWCASE_KEYS_DRAWING[0],
+                // drawing pointer moved
+                onPanUpdate: (details) {
+                  setState(() {
+                    RenderBox renderBox = context.findRenderObject();
+                    Offset point =
+                        renderBox.globalToLocal(details.localPosition);
+                    points[points.length - 1].add(point);
+                  });
+                },
+                // started drawing
+                onPanStart: (details) {
+                  setState(() {
+                    points.add([]);
+                    RenderBox renderBox = context.findRenderObject();
+                    Offset point =
+                        renderBox.globalToLocal(details.localPosition);
+                    points[points.length - 1].add(point);
+                  });
+                },
+                // finished drawing a stroke
+                onPanEnd: (details) async {
+                  // remove single points and don't run inference for them
+                  if (points[points.length - 1].length == 1)
+                    points.removeLast();
+                  else
+                    predictions = await canvas.runInference();
+                  setState(() {});
+                },
+                child: CustomPaint(
+                  painter: canvas,
+                ),
               ),
             ),
-          ),
-          Spacer(),
-          // undo/clear button
-          
-          Container(
-            width: canvasSize,
-            child:
-          Row(children: [
-            // undo
-            IconButton(
-              key: SHOWCASE_KEYS_DRAWING[1],
-              icon: Icon(Icons.undo),
-              onPressed: () async {
-                //only run inference if canvas still has strokes
-                if(points.length > 1){
-                  points.removeLast();
-                  predictions = await canvas.runInference();
-                }
-                else{
-                  points.removeLast();
-                  predictions = List.generate(10, (i) => " ");
-                }
-                setState(() {});
-              }
+            Spacer(),
+            // undo/clear button
+            Container(
+              width: canvasSize,
+              child: Row(
+                children: [
+                  // undo
+                  IconButton(
+                    key: SHOWCASE_KEYS_DRAWING[1],
+                    icon: Icon(Icons.undo),
+                    onPressed: () async {
+                      //only run inference if canvas still has strokes
+                      if(points.length > 1){
+                        points.removeLast();
+                        predictions = await canvas.runInference();
+                      }
+                      else{
+                        points.removeLast();
+                        predictions = List.generate(10, (i) => " ");
+                      }
+                      setState(() {});
+                    }
+                  ),
+                  // multi character search input
+                  Expanded(
+                      child: Center(
+                        child: KanjiBufferWidget(
+                          canvasSize: canvasSize,
+                          kanjiBuffer: kanjiBuffer,
+                        ),
+                      )
+                    //),
+                  ),
+                  // clear
+                  IconButton(
+                    key: SHOWCASE_KEYS_DRAWING[2],
+                    icon: Icon(Icons.clear),
+                    onPressed: () {
+                      setState(() {
+                        predictions = List.generate(10, (i) => " ");
+                        points.clear();
+                      });
+                    }
+                  ), 
+                ]
+              ),
+            ),
+            // prediction buttons
+            Container(
+              key: SHOWCASE_KEYS_DRAWING[3],
+              width: canvasSize,
+              // approximated button height (width/5) * numRows + padding  
+              height: (canvasSize / 5.0) * 2.0 + 10, 
+              child: GridView.count(
+                physics: new NeverScrollableScrollPhysics(),
+                crossAxisCount: 5,
+                crossAxisSpacing: 10,
+                mainAxisSpacing: 10,
+                children: List.generate(10, (i) {
+                  var ret;
+                  // instantiate the buttons which are used for showcase
+                  if(i < 2){
+                    ret = Container(
+                      key: SHOWCASE_KEYS_DRAWING[4+i],
+                      child: PredictionButton(
+                        char: predictions[i],
+                      )
+                    );
+                  }
+                  // instantiate the other buttons
+                  else ret = PredictionButton(char: predictions[i]);
+                  
+                  return GestureDetector(
+                    child: ret,
+                    onDoubleTap: (){
+                      setState(() {
+                        kanjiBuffer.kanjiBuffer += predictions[i];
+                      });
+                    },
+                  );
+                },
+                )
+              )
             ),
             Spacer(),
-            // clear
-            IconButton(
-              key: SHOWCASE_KEYS_DRAWING[2],
-              icon: Icon(Icons.clear),
-              onPressed: () {
-                setState(() {
-                  predictions = List.generate(10, (i) => " ");
-                  points.clear();
-                });
-              }
-            ), 
-          ]),
-          ),
-          // prediction buttons
-          Container(
-            key: SHOWCASE_KEYS_DRAWING[3],
-            width: canvasSize,
-            // approximated button height (width/5) * 2  
-            height: (MediaQuery.of(context).size.width / 5.0) * 2.0, 
-            child: GridView.count(
-              physics: new NeverScrollableScrollPhysics(),
-              crossAxisCount: 5,
-              crossAxisSpacing: 10,
-              mainAxisSpacing: 10,
-              children: List.generate(10, (i) { 
-                if(i < 2){
-                  return Container(
-                    key: SHOWCASE_KEYS_DRAWING[4+i],
-                      child: PredictionButton(
-                        predictions[i],
-                    )
-                  );
-                }
-                else return PredictionButton(predictions[i]);
-              },
-              )
-            )
-          ),
-          Spacer(),
-        ],
+          ],
+        ),
       )
-    )
     );
   }
   
