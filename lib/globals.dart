70723dba08d6349dc954919c00f1784308e5c793
dario <dario.klepoch@freenet.de>
2021-03-25 17:41:51 +0100

    refactored tutorial

--- lib/globals.dart
+++ lib/globals.dart
@@ -6,6 +6,7 @@ import 'package:tflite_flutter/tflite_flutter.dart';
 import 'package:tutorial_coach_mark/tutorial_coach_mark.dart';
 
 import 'Settings.dart';
+import 'ShowcaseTuple.dart';
 
 
 // the title of the app
@@ -13,6 +14,8 @@ const String APP_TITLE = "DaKanjiRecognizer";
 // the version number of this app
 // ignore: non_constant_identifier_names
 String VERSION;
+// all versions which implemented new features for the drawing screen
+List<String> drawingScreenNewFeatures = ["1.0.0", "1.1.0"];
 
 // the saved settings
 // ignore: non_constant_identifier_names
@@ -24,31 +27,46 @@ final GlobalKey<ScaffoldState> DRAWER_KEY = GlobalKey();
 
 // showcase view keys
 // ignore: non_constant_identifier_names
-final List<GlobalKey> SHOWCASE_KEYS_DRAWING =
-  List.generate(SHOWCASE_TEXTS_DRAWING.length, (i) => GlobalKey());
-const List<String> SHOWCASE_IDENTIFIERS_DRAWING = [
-  "Drawing",
-  "Undo",
-  "Clear",
-  "Predictions",
-  "Short Press",
-  "Long Press",
-  "Dictionary Settings"
-];
-const List<String> SHOWCASE_TEXTS_DRAWING = [
-  "Draw a character here.",
-  "Press to undo the last stroke.",
-  "Erase all strokes",
-  "The predicted characters will be shown here.",
-  "A short press copies the prediction.",
-  "A long press opens the prediction in a dictionary.",
-  "In the settings the translation service can be chosen."
-];
-// ignore: non_constant_identifier_names
-final List<ContentAlign> SHOWCASE_ALIGNS_DRAWING = [
-  ContentAlign.bottom, ContentAlign.bottom, ContentAlign.bottom,
-  ContentAlign.top, ContentAlign.bottom, ContentAlign.bottom,
-  ContentAlign.bottom
+final List<ShowcaseTuple> SHOWCASE_DRAWING = [
+  // 0 - Drawing canvas
+  ShowcaseTuple(GlobalKey(), "Drawing",
+    "Draw a character here", ContentAlign.bottom),
+  // 1 - undo last stroke
+  ShowcaseTuple(GlobalKey(), "Undo", 
+    "Press to undo the last stroke", ContentAlign.bottom), 
+  // 2 - clear canvas
+  ShowcaseTuple(GlobalKey(), "Clear", 
+    "Erase all strokes", ContentAlign.bottom), 
+  // 3 - prediction buttons
+  ShowcaseTuple(GlobalKey(), "Predictions", 
+    "The predicted characters will be shown here", ContentAlign.top), 
+  // 4 - short press prediction button
+  ShowcaseTuple(GlobalKey(), "Short Press Prediction",
+    "A short press copies the prediction", ContentAlign.bottom), 
+  // 5 - long press prediction button
+  ShowcaseTuple(GlobalKey(), "Long Press Prediction",
+    "A long press opens the prediction in a dictionary", ContentAlign.bottom), 
+  // 6 - multi search box
+  ShowcaseTuple(GlobalKey(), "Multi search",
+    "Here you can search multiple characters at once", ContentAlign.bottom), 
+  // 7 - double tap prediction button
+  ShowcaseTuple(GlobalKey(), "Double Tap Prediction",
+    "A Double Tap adds the character to the search box", ContentAlign.bottom), 
+  // 8 - multi search short press 
+  ShowcaseTuple(GlobalKey(), "Multi search short press",
+    "A short press copies the characters to the clipboard", ContentAlign.bottom), 
+  // 9 - multi search long press
+  ShowcaseTuple(GlobalKey(), "Multi search long press",
+    "A long press opens the characters in a dictionary", ContentAlign.bottom), 
+  // 10 - multi search double tap
+  ShowcaseTuple(GlobalKey(), "Multi search double tap",
+    "A double tap empties the field", ContentAlign.bottom), 
+  // 11 - multi search swipe left
+  ShowcaseTuple(GlobalKey(), "Multi search swipe left",
+    "Swiping left on this field deletes the last character", ContentAlign.bottom), 
+  // 12 - change dict in settings
+  ShowcaseTuple(GlobalKey(), "Dictionary Settings",
+    "In the settings the translation service can be chosen", ContentAlign.bottom), 
 ];
 // ignore: non_constant_identifier_names
 final Color SHOWCASE_VIGNETTE_COLOR = Color.fromARGB(255, 10, 10, 10);
