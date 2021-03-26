3a10eee5113402fad106997d813cc69dcc7bc488
dario <dario.klepoch@freenet.de>
2021-03-26 19:59:59 +0100

    intermediate page to handle whats new popup

---
+++ lib/HomeScreen.dart
@@ -0,0 +1,75 @@
+import 'package:da_kanji_recognizer_mobile/DrawScreen.dart';
+import 'package:da_kanji_recognizer_mobile/globals.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_markdown/flutter_markdown.dart';
+
+
+
+class HomeScreen extends StatefulWidget {
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+
+  @override
+  void initState() { 
+    super.initState();
+
+    // after the page was build open the what's new pop up
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+      if(SHOW_CHANGELOG){
+        SHOW_CHANGELOG = false;
+    
+        showDialog(
+          context: context,
+          builder: (BuildContext context){ 
+            return SimpleDialog(
+              title: Center(child: Text("🎉 What's new 🎉")),
+              children: [
+                Container(
+                  padding: EdgeInsets.all(10),
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        child: Markdown(data: NEW_CHANGELOG,),
+                        width: MediaQuery.of(context).size.width * 3/4,
+                        height: MediaQuery.of(context).size.height * 2/4,
+                      ),
+                      OutlinedButton(
+                          //Colors.white.withAlpha(50),
+                        onPressed: () async {
+                          Navigator.pushNamedAndRemoveUntil(
+                            context, "/home", (Route<dynamic> route) => false);
+                        },
+                        child: Text("close")
+                      )
+                    ]
+                  )
+                )
+              ],
+            );
+          }
+        ).then((value) {
+          Navigator.pushNamedAndRemoveUntil(
+            context, "/home", (Route<dynamic> route) => false);
+        });
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    // show changelog pop up when a new version is being used
+    if(SHOW_CHANGELOG){
+      return Scaffold();
+    }
+    // otherwise show the drawing screen
+    else{
+      return DrawScreen();
+    }
+  }
+}
