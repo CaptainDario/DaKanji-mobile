8f6e1690d294f4c124c1732c82eb2bf343288416
dario <dario.klepoch@freenet.de>
2021-04-06 18:53:37 +0200

    added DaKanji banner

--- lib/DaKanjiDrawer.dart
+++ lib/DaKanjiDrawer.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 import 'globals.dart';
@@ -16,17 +18,30 @@ class DaKanjiDrawer extends StatelessWidget {
       child: ListView(
         padding: EdgeInsets.zero,
         children: <Widget>[
-          DrawerHeader(child: Text("DaKanji")),
-          
+          SizedBox(
+            height: MediaQuery.of(context).padding.top + 64 + 20,
+            child: DrawerHeader(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children:[
+                  Image( height: 84, image: AssetImage("media/banner.png"),),
+                ]
+              ),
+              margin: EdgeInsets.all(0),
+              padding: EdgeInsets.all(0),
+            ),
+          ),
+
           // Drawer entry to go to the Kanji drawing screen
           ListTile(
             leading: Icon(Icons.brush_outlined),
             title: Text("Drawing"),
             onTap: () {
               print(ModalRoute.of(context).settings.name);
-              if(ModalRoute.of(context).settings.name != "/drawing")
+              if(ModalRoute.of(context).settings.name != "/drawing"){
                 Navigator.pushNamedAndRemoveUntil(
                   context, "/drawing", (Route<dynamic> route) => false);
+              }
             },
           ),
 
