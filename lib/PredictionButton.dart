94521b1e06581d8bfda9e36b8003c45f61e285b4
dario <dario.klepoch@freenet.de>
2021-04-18 20:08:26 +0200

    animated double tap

--- lib/PredictionButton.dart
+++ lib/PredictionButton.dart
@@ -6,39 +6,90 @@ import 'HandlePredictions.dart';
 /// A button which shows the given [char].
 /// 
 /// It can copy [char] to the clipboard or open it in a dictionary.
-class PredictionButton extends StatelessWidget {
+class PredictionButton extends StatefulWidget {
+
   
   final String char;
+  final void Function() tapFunction;
+  PredictionButton (this.char, this.tapFunction);
+  
+  @override
+  _PredictionButtonState createState() => _PredictionButtonState();
+}
 
+class _PredictionButtonState extends State<PredictionButton>
+  with TickerProviderStateMixin{
+    
+  AnimationController controller;
+  Animation<double> animation;
 
-  PredictionButton({this.char});
+  void anim(){
+   controller.forward(from: 0.0); 
+  }
 
+  @override
+  void initState() { 
+    super.initState();
+    
+    controller = AnimationController(
+      duration: const Duration(milliseconds: 100),
+      vsync: this,
+    );
+    animation = new Tween(
+      begin: 1.0,
+      end: 1.05,
+    ).animate(new CurvedAnimation(
+      parent: controller,
+      curve: Curves.decelerate,
+    ));
+
+    // run the animation always reversed after completion 
+    animation.addStatusListener((status) {
+      if(status == AnimationStatus.completed)
+        controller.reverse();
+    });
+  }
+
+  @override
+  void dispose() { 
+    controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context){
-  return Transform.scale(
-    scale: 0.9, 
-    child: AspectRatio(
-      aspectRatio: 1,
-      child: GestureDetector(
-        child: ElevatedButton(
-           
-          // handle a short press
-          onPressed: () {
-            HandlePrediction().handlePress(false, context, char);
+  return ScaleTransition(
+    scale: animation, 
+    child: Transform.scale(
+        scale: 0.9,
+        child: AspectRatio(
+        aspectRatio: 1,
+        child: GestureDetector(
+          behavior: HitTestBehavior.translucent,
+
+          onDoubleTap: () {
+            controller.forward(from: 0.0);
+            widget.tapFunction();
           },
-          
-          // handle a long press 
-          onLongPress: () async {
-            HandlePrediction().handlePress(true, context, char);
-          },
-          child: FittedBox(
-            child: Text(
-              char,
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 1000.0,
-              ),
+
+          child: ElevatedButton(
+            // handle a short press
+            onPressed: () {
+              HandlePrediction().handlePress(false, context, widget.char);
+            },
+            
+            // handle a long press 
+            onLongPress: () async {
+              HandlePrediction().handlePress(true, context, widget.char);
+            },
+            child: FittedBox(
+              child: Text(
+                widget.char,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 1000.0,
+                ),
+              )
             )
           )
         )
