0c7e4028d37d5e2f6bcd7b14ca0abf9725716a92
dario <dario.klepoch@freenet.de>
2021-05-06 19:33:58 +0200

    notifier for character lookup

--- lib/provider/Lookup.dart
+++ lib/provider/Lookup.dart
@@ -1,29 +1,53 @@
-
 import 'package:flutter/foundation.dart';
+
+import 'package:da_kanji_mobile/model/helper/HandlePredictions.dart';
+
+
 
 /// Class which notifies its listeners always when [_value] changed.
 class Lookup with ChangeNotifier{
   
-  /// the character which be searched in a dictionary
+  /// the character(s) which can be searched in a dictionary
   String _chars;
-
-  /// is the animation for adding a new character running
+  /// the dictionary URL for looking up [_chars]
   String _url;
+  /// are the characters which will be looked up from the kanjibuffer
+  bool _buffer;
+  /// was the lookup started with long press
+  bool _longPress;
+
 
   /// initializes a new [KanjiBuffer] instance
   Lookup() {
     _chars = "";
     _url = "";
+    _buffer = false;
+    _longPress = false;
   }
 
-  /// returns the current value
   String get chars{
     return _chars;
   }
 
+  String get url{
+    return _url;
+  }
+
+  bool get buffer{
+    return _buffer;
+  }
+
+  bool get longPress{
+    return _longPress;
+  }
+
   /// set the current value to [value] and notify listeners
-  set chars(String value){
+  void setChar(String value,
+    {bool buffer = false, bool longPress = false}){
     _chars = value;
+    _buffer = buffer;
+    _longPress = longPress;
+    _url = HandlePrediction().openWithSelectedDictionary(_chars);
     notifyListeners();
   }
 
