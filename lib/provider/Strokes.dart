1c725e820adbe70b3b8901f9065c7ce44c6cfd61
dario <dario.klepoch@freenet.de>
2021-05-16 23:18:34 +0200

    count the sub paths as separate attribute

--- lib/provider/Strokes.dart
+++ lib/provider/Strokes.dart
@@ -5,7 +5,11 @@ import 'package:flutter/widgets.dart';
 
 class Strokes with ChangeNotifier{
 
+  /// The path object of all strokes
   Path _path;
+  /// How many strokes are there
+  int _strokeCount; 
+
 
   get path {
     return _path;
@@ -16,17 +20,33 @@ class Strokes with ChangeNotifier{
 
     notifyListeners();
   }
+  
+  get strokeCount {
+    return _strokeCount;
+  }
+
+  void incrementStrokeCount(){
+    _strokeCount++;
+  }
+  
+  void decrementStrokeCount(){
+    _strokeCount--;
+  }
 
   Strokes() {
     _path = Path();
+    _strokeCount = 0;
   }
 
+  /// Deletes all drawn strokes.
   void deleteAllStrokes(){
     _path.reset();
+    _strokeCount = 0;
 
     notifyListeners();
   }
 
+  /// Deletes the last stroke of all drawn strokes.
   void removeLastStroke(){
     
     // get all strokes except for the last one
@@ -37,6 +57,8 @@ class Strokes with ChangeNotifier{
       newPath.addPath(element.extractPath(0, double.infinity), Offset.zero);
     });
     _path = newPath;
+
+    decrementStrokeCount();
 
     notifyListeners();
   }
