a6a0407b0b23a2e8f30c6d25232ecbf05e1895f7
dario <dario.klepoch@freenet.de>
2021-06-27 19:51:17 +0200

    added localization

---
+++ lib/locales_json.dart
@@ -0,0 +1,28 @@
+// DO NOT EDIT. This is code generated via package:easy_localization/generate.dart
+
+// ignore_for_file: prefer_single_quotes
+
+import 'dart:ui';
+
+import 'package:easy_localization/easy_localization.dart' show AssetLoader;
+
+class CodegenLoader extends AssetLoader{
+  const CodegenLoader();
+
+  @override
+  Future<Map<String, dynamic>> load(String fullPath, Locale locale ) {
+    return Future.value(mapLocales[locale.toString()]);
+  }
+
+  static const Map<String,dynamic> en = {
+  "HomeScreen": {
+    "RatePopup": {
+      "text": "Do you like DaKanji?\nIf that is the case a rating would be awesome and help this project alot!",
+      "dont_ask_again": "Do not ask again",
+      "rate": "Rate",
+      "close": "close"
+    }
+  }
+};
+static const Map<String, Map<String,dynamic>> mapLocales = {"en": en};
+}
---
+++ lib/locales_keys.dart
@@ -0,0 +1,11 @@
+// DO NOT EDIT. This is code generated via package:easy_localization/generate.dart
+
+abstract class  LocaleKeys {
+  static const HomeScreen_RatePopup_text = 'HomeScreen.RatePopup.text';
+  static const HomeScreen_RatePopup_dont_ask_again = 'HomeScreen.RatePopup.dont_ask_again';
+  static const HomeScreen_RatePopup_rate = 'HomeScreen.RatePopup.rate';
+  static const HomeScreen_RatePopup_close = 'HomeScreen.RatePopup.close';
+  static const HomeScreen_RatePopup = 'HomeScreen.RatePopup';
+  static const HomeScreen = 'HomeScreen';
+
+}
