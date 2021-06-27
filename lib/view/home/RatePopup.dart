e8a2eb54a735002e1cd1f464c43b154187c4fbed
dario <dario.klepoch@freenet.de>
2021-06-27 21:43:54 +0200

    popup to ask the user for a rating

--- lib/view/home/RatePopup.dart
+++ lib/view/home/RatePopup.dart
@@ -1,6 +1,13 @@
+import 'package:da_kanji_mobile/provider/PlatformDependentVariables.dart';
+import 'package:da_kanji_mobile/provider/UserData.dart';
 import 'package:flutter/material.dart';
 
 import 'package:awesome_dialog/awesome_dialog.dart';
+import 'package:easy_localization/easy_localization.dart';
+
+import 'package:da_kanji_mobile/locales_keys.dart';
+import 'package:get_it/get_it.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 
 /// Shows a rate popup which lets the user rate the app on the platform specific
@@ -10,12 +17,76 @@ import 'package:awesome_dialog/awesome_dialog.dart';
 /// enables the option for the user to not show the rate popup again.
 void showRatePopup(BuildContext context, bool hasDoNotShowOption){
 
+  
+  GetIt.I<UserData>().rateDialogueWasShown = true;
+
   AwesomeDialog(
     context: context,
     animType: AnimType.SCALE,
-    dialogType: DialogType.INFO,
+    dialogType: DialogType.QUESTION,
     headerAnimationLoop: false,
-    body: Text("test")
+    body: Center(
+      child: Column(
+        //crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          // ask for a rating text
+          Text(
+            LocaleKeys.HomeScreen_RatePopup_text.tr(),
+            textAlign: TextAlign.center,
+          ),
+          SizedBox(height: 50,),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              // close button
+              ElevatedButton(
+                style: ButtonStyle(
+                  backgroundColor: 
+                    MaterialStateProperty.all(
+                      Color.fromARGB(100, 150, 150, 150)
+                    )
+                ),
+                onPressed: () {
+                  Navigator.pushNamedAndRemoveUntil(
+                    context, "/home", (Route<dynamic> route) => false);
+                },
+                child: Text(LocaleKeys.HomeScreen_RatePopup_close.tr())
+              ),
+              // do not ask again button
+              if(hasDoNotShowOption)
+                ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor: 
+                      MaterialStateProperty.all(
+                        Color.fromARGB(100, 150, 150, 150)
+                      )
+                  ),
+                  onPressed: () {
+                    GetIt.I<UserData>().doNotShowRateAgain = true;
+                    GetIt.I<UserData>().save();
+                    Navigator.pushNamedAndRemoveUntil(
+                      context, "/home", (Route<dynamic> route) => false);
+                  },
+                  child: Text(LocaleKeys.HomeScreen_RatePopup_dont_ask_again.tr())
+                ),
+              // rate button
+              ElevatedButton(
+                style: ButtonStyle(
+                  backgroundColor: 
+                    MaterialStateProperty.all(
+                      Color.fromARGB(100, 150, 150, 150)
+                    )
+                ),
+                onPressed: () async {
+                  launch(GetIt.I<PlatformDependentVariables>().appStoreLink);
+                },
+                child: Text(LocaleKeys.HomeScreen_RatePopup_rate.tr())
+              ),
+            ],
+          )
+        ],
+      )
+    )
   )..show();
 
 }
