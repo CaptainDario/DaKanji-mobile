55197c06a53aeeef7e6b97797ca411f564dfc7bb
dario <dario.klepoch@freenet.de>
2021-06-28 15:38:09 +0200

    updated localization

--- lib/locales_json.dart
+++ lib/locales_json.dart
@@ -18,42 +18,37 @@ class CodegenLoader extends AssetLoader{
   "DrawScreen": {
     "title": "Zeichnen",
     "tutorial": {
-      "clear_text": "Lösche all Striche",
-      "dictionary_settings_text": "In den Einstellung kann die Methode zum übersetzen gewählt werden",
-      "double_tap_prediction_text": "Tippe doppelt um das Zeichen der Suchbox hinzuzufügen",
-      "drawing_text": "Male ein Zeichen hier",
-      "long_press_prediction_text": "Lange auf eine Vorhersage drücken öffnet sie in einem Wörterbuch",
-      "multi_search_double_tap_text": "Doppelt auf das Feld zutippen löscht den Inhalt",
-      "multi_search_long_press_text": "Lange auf die Suchbox zu klicken öffnet sie in einem Wörterbuch",
-      "multi_search_short_press_text": "Kurz auf die Suchbox zu drücken kopiert die Zeichen in den Zwischenspeicher",
-      "multi_search_swipe_left_text": "Links auf der Suchbox zu swipen löscht das letzte Zeichen",
-      "multi_search_text": "Mit dieser Suchbox können mehrere Zeichen auf einmal gesucht werden",
-      "predictions_text": "Die vorhergesagten Zeichen werden hier angezeigt",
-      "short_press_prediction_text": "Einmal kurz drücken kopiert das vorhergesagte Zeichen",
+      "clear": "Lösche all Striche",
+      "double_tap_prediction": "Tippe doppelt um das Zeichen der Suchbox hinzuzufügen",
+      "drawing": "Male ein Zeichen hier",
+      "long_press_prediction": "Lange auf eine Vorhersage drücken öffnet sie in einem Wörterbuch",
+      "multi_search_double_tap": "Doppelt auf das Feld zutippen löscht den Inhalt",
+      "multi_search_long_press": "Lange auf die Suchbox zu klicken öffnet sie in einem Wörterbuch",
+      "multi_search_short_press": "Kurz auf die Suchbox zu drücken kopiert die Zeichen in den Zwischenspeicher",
+      "multi_search_swipe_left": "Links auf der Suchbox zu swipen löscht das letzte Zeichen",
+      "multi_search": "Mit dieser Suchbox können mehrere Zeichen auf einmal gesucht werden",
+      "predictions": "Die vorhergesagten Zeichen werden hier angezeigt",
+      "short_press_prediction": "Einmal kurz drücken kopiert das vorhergesagte Zeichen",
       "skip": "ÜBERSPRINGEN",
-      "undo_text": "Macht den letzten Strich rückgängig"
+      "undo": "Macht den letzten Strich rückgängig",
+      "dictionary_settings": "In den Einstellung kann die Methode zum übersetzen gewählt werden"
     }
   },
   "SettingsScreen": {
     "custom_url_hint": "URL von dem Wörterbuch das benutzt werden soll",
-    "dictionary_app": "App",
-    "dictionary_custom_url": "selbstdefinierte URL",
     "dictionary_system_translator": "betriebssysteminternes Übersetzer",
-    "dictionary_web": "Web",
     "drawing_title": "Zeichnen",
     "empty_canvas_after_double_tap": "Lösche alle Striche nach doppeltem Tippen",
-    "invert_short_long_press": "invertiere kurzes und langes drücken",
+    "invert_short_long_press": "Invertiere kurzes und langes drücken",
     "long_press_opens": "Langes drücken öffnet",
     "miscellaneous_title": "Verschiedenes",
     "show_tutorial": "Zeige Tutorial erneut (startet app neu)",
-    "theme": "Thema",
-    "themes_dark": "dunkel",
-    "themes_light": "hell",
-    "themes_system": "System",
+    "theme": "Thema (@:restarts_app)",
     "title": "Einstellungen",
     "use_default_browser_for_online_dictionaries": "Benutze den standard Webbrowser vom Betriebssystem",
     "custom_url_format": "Benutzerdefiniertes URL format",
-    "custom_url_explanation": "Die App ersetzt einen Platzhalter in der gegbenen URL mit dem vorhergesagten Zeichen. Dieser Plathalter ist: {kanjiPlaceholder}\n\nBeispiel:\nDas vorhergesagte Zeichen ist: '口' und man wil es auf 'jisho.org' nachschlagen. Dafür muss als erste die URL der website, welche für das Suchen benutzt wird, herausgefunden werden. Für 'jisho.org' ist das: 'https://jisho.org/search/口'. Nun muss nur noch das Zeichen aus der URL mit dem Platzhalter ersetzt werden. Das führt zu 'https://jisho.org/search/{kanjiPlaceholder}'."
+    "custom_url_explanation": "Die App ersetzt einen Platzhalter in der gegbenen URL mit dem vorhergesagten Zeichen. Dieser Plathalter ist: {kanjiPlaceholder}\n\nBeispiel:\nDas vorhergesagte Zeichen ist: '口' und man wil es auf 'jisho.org' nachschlagen. Dafür muss als erste die URL der website, welche für das Suchen benutzt wird, herausgefunden werden. Für 'jisho.org' ist das: 'https://jisho.org/search/口'. Nun muss nur noch das Zeichen aus der URL mit dem Platzhalter ersetzt werden. Das führt zu 'https://jisho.org/search/{kanjiPlaceholder}'.",
+    "language": "Sprache"
   },
   "HomeScreen": {
     "RatePopup": {
@@ -62,7 +57,14 @@ class CodegenLoader extends AssetLoader{
       "rate": "Bewerten",
       "text": "Es sieht so aus als würde dir DaKanji gefallen.\nFalls das stimmt würde eine Bewertung dem Projekt extrem weiter helfen!"
     }
-  }
+  },
+  "app": "App",
+  "web": "Web",
+  "custom_url": "selbstdefinierte URL",
+  "restarts_app": "startet app neu",
+  "dark": "Dunkel",
+  "light": "Hell",
+  "system": "System"
 };
 static const Map<String,dynamic> en = {
   "HomeScreen": {
@@ -76,20 +78,20 @@ static const Map<String,dynamic> en = {
   "DrawScreen": {
     "title": "Drawing",
     "tutorial": {
-      "clear_text": "Erases all strokes",
-      "drawing_text": "Draw a character here",
-      "undo_text": "Press to undo the last stroke",
-      "predictions_text": "The predicted characters will be shown here",
-      "short_press_prediction_text": "A short press copies the prediction",
-      "long_press_prediction_text": "A long press opens the prediction in a dictionary",
-      "multi_search_text": "Here you can search multiple characters at once",
-      "double_tap_prediction_text": "A Double Tap adds the character to the search box",
-      "multi_search_short_press_text": "A short press copies the characters to the clipboard",
+      "clear": "Erases all strokes",
+      "drawing": "Draw a character here",
+      "undo": "Press to undo the last stroke",
+      "predictions": "The predicted characters will be shown here",
+      "short_press_prediction": "A short press copies the prediction",
+      "long_press_prediction": "A long press opens the prediction in a dictionary",
+      "multi_search": "Here you can search multiple characters at once",
+      "double_tap_prediction": "A Double Tap adds the character to the search box",
+      "multi_search_short_press": "A short press copies the characters to the clipboard",
       "skip": "SKIP",
-      "multi_search_long_press_text": "A long press opens the characters in a dictionary",
-      "multi_search_double_tap_text": "A double tap empties the field",
-      "multi_search_swipe_left_text": "Swiping left on this field deletes the last character",
-      "dictionary_settings_text": "In the settings the translation service can be chosen"
+      "multi_search_long_press": "A long press opens the characters in a dictionary",
+      "multi_search_double_tap": "A double tap empties the field",
+      "multi_search_swipe_left": "Swiping left on this field deletes the last character",
+      "dictionary_settings": "In the settings the translation service can be chosen"
     }
   },
   "SettingsScreen": {
@@ -102,17 +104,19 @@ static const Map<String,dynamic> en = {
     "empty_canvas_after_double_tap": "Empty canvas after double tap",
     "use_default_browser_for_online_dictionaries": "Use OS's default browser for online dictionaries",
     "miscellaneous_title": "Miscellaneous",
-    "theme": "theme (restarts app)",
+    "theme": "theme (@:restarts_app)",
     "show_tutorial": "Reshow tutorial (restarts app)",
-    "themes_light": "light",
-    "themes_dark": "dark",
-    "themes_system": "system",
-    "dictionary_app": "app",
-    "dictionary_custom_url": "a custom URL",
-    "dictionary_web": "web",
     "title": "Settings",
-    "dictionary_system_translator": "OS integrated translator"
-  }
+    "dictionary_system_translator": "OS integrated translator",
+    "language": "Language"
+  },
+  "app": "app",
+  "web": "web",
+  "custom_url": "custom URL",
+  "restarts_app": "restarts app",
+  "dark": "dark",
+  "light": "light",
+  "system": "system"
 };
 static const Map<String, Map<String,dynamic>> mapLocales = {"de": de, "en": en};
 }
--- lib/locales_keys.dart
+++ lib/locales_keys.dart
@@ -2,9 +2,49 @@
 
 abstract class  LocaleKeys {
   static const DrawScreen_title = 'DrawScreen.title';
-  static const DrawScreen_tutorial_clear_text = 'DrawScreen.tutorial.clear_text';
-  static const DrawScreen_tutorial_clear_title = 'DrawScreen.tutorial.clear_title';
+  static const DrawScreen_tutorial_clear = 'DrawScreen.tutorial.clear';
+  static const DrawScreen_tutorial_double_tap_prediction = 'DrawScreen.tutorial.double_tap_prediction';
+  static const DrawScreen_tutorial_drawing = 'DrawScreen.tutorial.drawing';
+  static const DrawScreen_tutorial_long_press_prediction = 'DrawScreen.tutorial.long_press_prediction';
+  static const DrawScreen_tutorial_multi_search_double_tap = 'DrawScreen.tutorial.multi_search_double_tap';
+  static const DrawScreen_tutorial_multi_search_long_press = 'DrawScreen.tutorial.multi_search_long_press';
+  static const DrawScreen_tutorial_multi_search_short_press = 'DrawScreen.tutorial.multi_search_short_press';
+  static const DrawScreen_tutorial_multi_search_swipe_left = 'DrawScreen.tutorial.multi_search_swipe_left';
+  static const DrawScreen_tutorial_multi_search = 'DrawScreen.tutorial.multi_search';
+  static const DrawScreen_tutorial_predictions = 'DrawScreen.tutorial.predictions';
+  static const DrawScreen_tutorial_short_press_prediction = 'DrawScreen.tutorial.short_press_prediction';
+  static const DrawScreen_tutorial_skip = 'DrawScreen.tutorial.skip';
+  static const DrawScreen_tutorial_undo = 'DrawScreen.tutorial.undo';
+  static const DrawScreen_tutorial_dictionary_settings = 'DrawScreen.tutorial.dictionary_settings';
   static const DrawScreen_tutorial = 'DrawScreen.tutorial';
   static const DrawScreen = 'DrawScreen';
+  static const SettingsScreen_custom_url_hint = 'SettingsScreen.custom_url_hint';
+  static const SettingsScreen_dictionary_system_translator = 'SettingsScreen.dictionary_system_translator';
+  static const SettingsScreen_drawing_title = 'SettingsScreen.drawing_title';
+  static const SettingsScreen_empty_canvas_after_double_tap = 'SettingsScreen.empty_canvas_after_double_tap';
+  static const SettingsScreen_invert_short_long_press = 'SettingsScreen.invert_short_long_press';
+  static const SettingsScreen_long_press_opens = 'SettingsScreen.long_press_opens';
+  static const SettingsScreen_miscellaneous_title = 'SettingsScreen.miscellaneous_title';
+  static const SettingsScreen_show_tutorial = 'SettingsScreen.show_tutorial';
+  static const SettingsScreen_theme = 'SettingsScreen.theme';
+  static const SettingsScreen_title = 'SettingsScreen.title';
+  static const SettingsScreen_use_default_browser_for_online_dictionaries = 'SettingsScreen.use_default_browser_for_online_dictionaries';
+  static const SettingsScreen_custom_url_format = 'SettingsScreen.custom_url_format';
+  static const SettingsScreen_custom_url_explanation = 'SettingsScreen.custom_url_explanation';
+  static const SettingsScreen_language = 'SettingsScreen.language';
+  static const SettingsScreen = 'SettingsScreen';
+  static const HomeScreen_RatePopup_close = 'HomeScreen.RatePopup.close';
+  static const HomeScreen_RatePopup_dont_ask_again = 'HomeScreen.RatePopup.dont_ask_again';
+  static const HomeScreen_RatePopup_rate = 'HomeScreen.RatePopup.rate';
+  static const HomeScreen_RatePopup_text = 'HomeScreen.RatePopup.text';
+  static const HomeScreen_RatePopup = 'HomeScreen.RatePopup';
+  static const HomeScreen = 'HomeScreen';
+  static const app = 'app';
+  static const web = 'web';
+  static const custom_url = 'custom_url';
+  static const restarts_app = 'restarts_app';
+  static const dark = 'dark';
+  static const light = 'light';
+  static const system = 'system';
 
 }
